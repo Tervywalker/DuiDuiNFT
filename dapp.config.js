277a74fdb4447878d9dc1d2ff2aab88845f6b5ee
Tervywalker <102008113+Tervywalker@users.noreply.github.com>
2022-10-08 03:02:10 +0800

    Add files via upload

---
+++ dapp.config.js
@@ -0,0 +1,10 @@
+const config = {
+    title: 'DuiDui 妖精の旅',
+    description: 'A showcase dapp which is built for you to create your own',
+    contractAddress: '0x578e712086b29e6C72875CD8c09FA7D4b8547B50',
+    maxMintAmount: 5,
+    presaleMaxMintAmount: 2,
+    price: 0.01
+  }
+
+export { config }
