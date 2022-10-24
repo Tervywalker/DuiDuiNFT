8349d968d04d11774866c4236a3ffdd1cf477b75
Tervywalker <102008113+Tervywalker@users.noreply.github.com>
2022-10-25 00:59:09 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -93,7 +93,7 @@ export default function Home() {
 
               <li className="cursor-pointer my-3 md:my-0">
                 <a
-                  href="https://discord.com/invite/Pkxqb2Pkbg"
+                  href="https://t.co/D2MEGdsvRQ"
                   target="_blank"
                   rel="noreferrer"
                 >
